2cd6181d6fc2b1a81041222ed9363d8e5ed91296
shlommi <shlommi@gmail.com>
2022-06-22 22:59:28 +0300

    Update App.js
    adding default color on entry screen

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Values from 'values.js';
 function App() {
   const [color, setColor] = useState('');
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values('#bada55').all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
